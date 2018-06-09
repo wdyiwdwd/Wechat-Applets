0790619ce706459154da716cd01dbedffef6f4e2
gyg <1511423@mail.nankai.edu.cn>
2018-06-09 21:00:28 +0800

    GYG,fix bugs of join group.

---
+++ server/api/joinGroup.js
@@ -0,0 +1,20 @@
+var Group = require('../models').Group;
+var User = require('../models').User;
+var GroupUser = require('../models').GroupUser;
+
+exports.joinGroup = async function (req, res) {
+  var users = await User.findAll({
+    'where': {
+      'wxid': req.query.wxid
+    }
+  });
+  var groups = await Group.findAll({
+    'where': {
+      'groupid': req.query.openGId
+    }
+  });
+  if (users.length > 0) {
+    await groups[0].addUser(user[0]);
+  } 
+  res.send('joinGroup success!');
+}
